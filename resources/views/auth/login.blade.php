5019e1ae2fb23c38016509e1133bf63f9846ae56
yudhaindra <yudha.indra@ti.ukdw.ac.id>
2024-12-03 18:27:42 +0700

    push baru with login

---
+++ resources/views/auth/login.blade.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Inter:wght@400;800;900&display=swap" />
+    <link rel="stylesheet" href="index.css" />
+  </head>
+  <body>
+    <div class="main-container">
+      <div class="rectangle">
+        <span class="sma-immanuel-kalasan">SMA IMMANUEL KALASAN</span>
+      </div>
+      <div class="rectangle-1">
+        <div class="head-n-foot"></div>
+        <span class="im-class">IM-CLASS</span>
+      </div>
+      <div class="head-n-foot-2"></div>
+      <div class="rectangle-3">
+        <span class="id">ID</span>
+        <div class="rectangle-4">
+          <span class="nomor-induk-siswa">Nomor Induk Siswa</span>
+        </div>
+        <span class="password">Password</span>
+        <div class="rectangle-5"><span class="password-6">Password</span></div>
+        <button class="button"><span class="login-button">Login</span></button>
+      </div>
+      <div class="footer">
+        <div class="content"></div>
+        <span class="contact-info"
+          >Jalan Solo KM 15 Gampar, Tamanmartani, Kalasan, Sleman, Yogyakarta
+          Telp : 0274 4469287 Email : smaimmanuelkalasan@gmail.com</span
+        >
+      </div>
+    </div>
+  </body>
+</html>
